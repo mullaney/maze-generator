5a2ecaebb06c2983161e3386ecbe4c4bd1a1df96
Kevin Kelly <kvinklly@gmail.com>
2017-11-05 21:07:17 -0600

    move to random unvisited neighbor

--- sketch.js
+++ sketch.js
@@ -1,11 +1,18 @@
 let cols, rows;
-const w = 40;
+const w = 20;
 let grid = [];
+let current;
+
+var getIndex = function(c, r) {
+	if (c < 0 || r < 0 || c >= cols || r >= rows) return -1;
+	return c * cols + r;
+}
 
 function setup() {
-	createCanvas(401, 401);
+	createCanvas(601, 601);
 	cols = floor(width / w);
 	rows = floor(height / w);
+	frameRate(30);
 
 	for(var c = 0; c < cols; c++) {
 		for(var r = 0; r< rows; r++) {
@@ -13,6 +20,7 @@ function setup() {
 			grid.push(cell);
 		}
 	}
+	current = grid[0];
 }
 
 function draw() {
@@ -20,22 +28,53 @@ function draw() {
 	for(var i = 0; i < grid.length; i++) {
 		grid[i].show();
 	}
+	current.visited = true;
+	var next = current.checkNeighbors();
+	if (next) {
+		next.visited = true;
+		current = next;
+	}
 }
 
 function Cell(col, row) {
 	this.col = col;
 	this.row = row;
 	this.walls = [true, true, true, true];
+	this.visited = false;
+
+	this.checkNeighbors = function() {
+		var neighbors = [];
+		var top    = grid[getIndex(this.col, this.row - 1)];
+		var right  = grid[getIndex(this.col + 1, this.row)];
+		var bottom = grid[getIndex(this.col, this.row + 1)];
+		var left   = grid[getIndex(this.col - 1, this.row)];
+
+		if (top && !top.visited) { neighbors.push(top); }
+		if (right && !right.visited) { neighbors.push(right); }
+		if (bottom && !bottom.visited) { neighbors.push(bottom); }
+		if (left && !left.visited) { neighbors.push(left); }
+
+		if (neighbors.length > 0) {
+			return neighbors[floor(random() * neighbors.length)];
+		} else {
+			return undefined;
+		}
+	}
 
 	this.show = () => {
 		var x = this.col * w;
 		var y = this.row * w;
-		stroke(0,255,255);
+		stroke(255,0,255);
 
-		if(this.walls[0]) { line(x,y,x+w,y); }
-		if(this.walls[1]) { line(x+w,y,x+w,y+w); }
-		if(this.walls[2]) { line(x+w,y+w,x,y+w); }
-		if(this.walls[3]) { line(x,y+w,x,y); }
+		if (this.walls[0]) { line(x,y,x+w,y); }
+		if (this.walls[1]) { line(x+w,y,x+w,y+w); }
+		if (this.walls[2]) { line(x+w,y+w,x,y+w); }
+		if (this.walls[3]) { line(x,y+w,x,y); }
+
+		if (this.visited) {
+			fill(255,0,255,110);
+			rect(x,y,w,w);
+		}
 
 
 		// noFill();
